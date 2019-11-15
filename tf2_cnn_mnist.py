4386481dc5e915662f81599ef9cafb289f3de0f9
曾令燊 <dvsseed@hotmail.com>
2019-11-15 21:57:32 +0800

    Update tf2_cnn_mnist.py

--- tf2_cnn_mnist.py
+++ tf2_cnn_mnist.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 from PIL import Image
 from sklearn.preprocessing import OneHotEncoder
 from pathlib import Path
